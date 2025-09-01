79593a14ecefc5892bd56ca801a98a793c3d1a28
emon22830 <mdemon22830@gmail.com>
2025-09-01 16:09:42 +0800

    Login Ui with Clerk Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import Discover from "./pages/Discover";
+import CreatePost from "./pages/CreatePost";
+import Messages from "./pages/Messages";
+import Connections from "./pages/Connections";
+import Profile from "./pages/Profile";
+import ChatBox from "./pages/ChatBox";
+import {useUser} from '@clerk/clerk-react'
+import Layout from "./pages/Layout";
 
 const App = () => {
+  const{user} = useUser()
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={!user?<Login />:<Layout/>} />
+        <Route path="feed" element={<Feed />} />
+        <Route path="messages" element={<Messages />} />
+        <Route path="messages/:userId" element={<ChatBox />} />
+        <Route path="connections" element={<Connections />} />
+        <Route path="discover" element={<Discover />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="profile/:profileId" element={<Profile />} />
+        <Route path="create-post" element={<CreatePost />} />
+      </Routes>
+    </>
   );
 };
 
